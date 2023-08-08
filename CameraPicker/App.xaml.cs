80ade7b50285401783789aead0935ca2811953d8
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2023-08-08 17:35:46 +0530

    Sample added.

---
+++ CameraPicker/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Syncfusion.Maui.ImageEditor;
+using System.Globalization;
+using System.Resources;
+
+namespace CameraPicker;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+        
+    }
+}
---
+++ CameraPicker/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace CameraPicker;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        Task task = GetImageEditorImageSourceAsync();
+        this.imageEditor.IsVisible = true;
+    }
+    private async Task GetImageEditorImageSourceAsync()
+    {
+        var status = await Permissions.CheckStatusAsync<Permissions.Camera>();
+        if (status != PermissionStatus.Granted)
+        {
+            // Camera permission is not granted, request it
+            status = await Permissions.RequestAsync<Permissions.Camera>();
+            if (status != PermissionStatus.Granted)
+            {
+                // Camera permission denied by the user
+                // You may show an alert or take appropriate action
+                return;
+            }
+        }
+
+        var photo = await MediaPicker.Default.CapturePhotoAsync();
+        if (photo != null)
+        {
+            // Get the stream of the captured photo and set it as the source of the Image Editor control.
+            imageEditor.Source = ImageSource.FromStream(() => photo.OpenReadAsync().Result);
+        }
+    }
+}
